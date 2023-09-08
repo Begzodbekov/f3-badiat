a3a25f599da532ee375f741f24c6ce8f99e84c11
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-09-08 18:12:09 +0500

    sdf

--- src/lib/bookData.js
+++ src/lib/bookData.js
@@ -2,20 +2,20 @@ export const books = [
           {
                     id:1,
                     adibId:1,
-                    name: "Otgan kunlar",
+                    name: "Qo'shiqlarim sizga",
                     img: "https://",
                     priceQ: "20000",
                     priceA: "15000",
                     priceE: "10000",
                     count: 1500,
-                    pages:300,
+                    pages:128,
                     category: "Tarixi",
                     nashiryot: "Hilol Nashiryoti"
           },
           {
                     id:2,
                     adibId:1,
-                    name: "Otgan kunlar",
+                    name: "To'xtab boraman",
                     img: "https://",
                     priceQ: "20000",
                     priceA: "15000",
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,14 +1,52 @@
+import Rahmonberdi from "../asests/img/rahmonberdi.png"
+import Abdulhamid from "../asests/img/Abdulhamid Cho'lpon.png"
 export const adibData = [
           {
                     id:1,
                     name:"Abdulla Avloniy",
-                    img:"https://asd",
-                    age: "1950 - 2013",
+                    img:"https://ilmlar.uz/wp-content/uploads/2021/09/abdulla-Avloniy-haqida-tarjimai-holi.jpeg",
+                    age: "1878-1934",
                     bookCount: '20',
-                    address:"Toshkent, Uzbekistan"
+                    address:"Toshkentda, Mergancha"
           },
           {
-                    id:1,
+                    id:2,
+                    name:"Nusrat Rahmat",
+                    img:"https://static.zarnews.uz/crop/1/c/720__80_1c8e767e101ea9caa034b9649bcc91cf.jpg?img=self&v=1589438338",
+                    age: "1933-1992",
+                    bookCount: '20',
+                    address:"Xorazm, Urganch"  
+          },
+          {
+                    id:3,
+                    name:"Rahmonberdi Madazimov",
+                    img:Rahmonberdi,
+                    age: "1926-1971",
+                    bookCount: '20',
+                    address:"Qoraqalpoqiston, Muynak"   
+          },
+          {
+                    id:4,
+                    name:"Hamza Hakimzoda Niyoziy",
+                    img:"https://ziyouz.uz/wp-content/uploads/2013/06/hamza-1.jpg",
+                    age: "1889-1929",
+                    bookCount: '20',
+                    address:"Namangan, Uchqoʻrgʻon"   
+          },
+          {
+                    id:5,
+                    name:"Abdulhamid Cho'lpon",
+                    img:Abdulhamid,
+                    age: "1896-1938",
+                    bookCount: '20',
+                    address:"Samarqand, O'zbekiston"   
+          },
+
+
+
+
+          {
+                    id:2,
                     name:"Abdulla Qodirov",
                     img:"https://asd",
                     age: "1950 - 2013",
