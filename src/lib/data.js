1e38608c1b47fc7732a75f02a6821ad6623965f8
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-09-08 16:06:15 +0500

    sfsd

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,10 +1,19 @@
 export const adibData = [
           {
                     id:1,
+                    name:"Abdulla Avloniy",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
+          {
+                    id:1,
                     name:"Abdulla Qodirov",
                     img:"https://asd",
                     age: "1950 - 2013",
                     bookCount: '20',
                     address:"Toshkent, Uzbekistan"
-          }
+          },
+
 ]
