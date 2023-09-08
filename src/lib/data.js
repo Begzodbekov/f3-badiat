dc16c08194022b37d871078e1688bacd6f3d9a95
ziyo2007 <qodirjonovz6@gmail.com>
2023-09-08 16:35:08 +0500

    fsfsf

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,3 +1,4 @@
+
 export const adibData = [
           {
                     id:1,
@@ -6,6 +7,28 @@ export const adibData = [
                     age: "1950 - 2013",
                     bookCount: '20',
                     address:"Toshkent, Uzbekistan"
+          },
+          {
+            id:16,
+            name:"Худайберди Тулпор ",
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR709kodTxD24nJ_xqmwCdLREgvq17lBcXsBGxdXTGN6R-JfEzkl64UtRgv2ePTaOSXhNE&usqp=CAU",
+            age: "(1877-1920) ",
+            bookCount: '10',
+            address:"Fergana, Uzbekistan"
+          },
+          {
+            id:16,
+            name:"Гулом Хайдаров ",
+            img:"https://referatlar.ucoz.net/_ph/1/2/607382499.jpg",
+            age:"(1917-1984)",
+            bookCount:10,
+            address:"Khorezm, Uzbekistan"
+          },
+          {
+            id:16, 
+            name:"Юсуф Рахмон",
+            img:""
           }
-          
+
+
 ]
