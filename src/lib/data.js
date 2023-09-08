bec6a2b18b9a3b8c261df3fe8b79342a44d12d98
ziyo2007 <qodirjonovz6@gmail.com>
2023-09-08 16:48:52 +0500

    hahahah

--- src/lib/data.js
+++ src/lib/data.js
@@ -27,8 +27,27 @@ export const adibData = [
           {
             id:16, 
             name:"Юсуф Рахмон",
-            img:""
-          }
+            img:"https://ziyouz.uz/wp-content/uploads/2013/10/ibrahim.jpg",
+            age: "(1912-1982)",
+            bookCount:10,
+            address:"Bukhara,Uzbekistan"
+          },
+          {
+            id:16,
+            name:"Хуршед Даврон",
+            img:"https://lh6.googleusercontent.com/-ZWzKfalazsE/Utp5uOGPGCI/AAAAAAAAKeY/A_9XUTckVFc/s332/0-XURSHID%252021.JPG",
+            age:" (1938-2004)",
+            bookCount:10,
+            address:"Samarkand, Uzbekistan"
+          },
+          {
+            id:16,
+            name:"Хайдар Алиев ",
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-tm2B3iyoduLctesuBSsu2DvJKahNtesiIg&usqp=CAU",
+            age:"(1919-2000)",
+            bookCount:10,
+            address:"Bukhara,Uzbekistan"
+          },
 
 
 ]
