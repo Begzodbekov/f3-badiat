d1d9317df8e7cabf8ca0fd7b2b20ec946041bc30
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-09-08 15:59:33 +0500

    sdsad

--- src/lib/data.js
+++ src/lib/data.js
@@ -6,5 +6,45 @@ export const adibData = [
                     age: "1950 - 2013",
                     bookCount: '20',
                     address:"Toshkent, Uzbekistan"
-          }
+          },
+            {
+                    id:6,
+                    name:"Abdurauf Fitrat",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
+            {
+                    id:7,
+                    name:"Abdulla Qodirov",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
+            {
+                    id:8,
+                    name:"Abdulla Qodirov",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
+            {
+                    id:9,
+                    name:"Abdulla Qodirov",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
+            {
+                    id:10,
+                    name:"Abdulla Qodirov",
+                    img:"https://asd",
+                    age: "1950 - 2013",
+                    bookCount: '20',
+                    address:"Toshkent, Uzbekistan"
+          },
 ]
