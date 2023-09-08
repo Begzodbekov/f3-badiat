c018673859156e123a3ce400559cee1b6453ab2c
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-09-08 15:48:15 +0500

    asdsad

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Signup.scss'
+import signUp from '../../asests/img/signup.png'
+
+function Signup() {
+  return (
+    <div className='signup'>
+      <div className="container">
+        <div className="wrapper">
+            <div className="left__wrap">
+                <img src={signUp} alt="" />
+                <h1>asdsaf</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
