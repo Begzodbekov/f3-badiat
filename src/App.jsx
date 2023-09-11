7c3d53c32fbfc5cac1bcf826e03bb4c473d20b4c
uzwhite444 <gocsgo398@gmail.com>
2023-09-11 16:12:31 +0500

    affg

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import Home from './pages/Home/Home';
 import Nasr from './pages/Nasr/Nasr';
 import Header from './Components/Header/Header';
+import Author from './pages/Author/Author';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/nasr' element ={<Nasr/>}></Route>
+        <Route path='/author' element={<Author/>}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Author/Author.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthorD from '../../Components/AuthorD/AuthorD'
+
+function Author() {
+  return (
+    <div>
+        <AuthorD/>
+    </div>
+  )
+}
+
+export default Author
