f4589423c3bbd09504bb23d23599a2d3a4ce517d
ortiqboy2005 <ortiqboy0804@gmail.com>
2023-09-11 16:25:51 +0500

    sada

---
+++ src/lib/bookData3.js
@@ -0,0 +1,31 @@
+export const bookData3 = [
+    {
+        id: 21,
+        adibId: 1,
+        name: "ko'zlaringda bormi , yulduzing",
+        img: "https://assets.asaxiy.uz/product/items/desktop/3713bdda7149579475f3734e8bd0e14a2022110612550553442e8Jt9jxGUq.jpg.webp",
+        priceQ: "25000",
+        priceA: "25000",
+        priceE: "25000",
+        count: 9,
+        pages: 300,
+        category: "Tarixi",
+        nashiryot: "Hilol Nashiryoti"
+    },
+    {
+        id: 22,
+        adibId: 2,
+        name: "",
+        img: "",
+        priceQ: "",
+        priceA: "",
+        priceE: "",
+        count: 9,
+        pages:9,
+        category: "",
+        nashiryot: ""
+    }, {
+
+    }
+
+]
