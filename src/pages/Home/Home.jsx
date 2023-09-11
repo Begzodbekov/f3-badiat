640a6b244d0efeae83a75ea06fb28a4aaa67cdc7
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-09-11 14:50:59 +0500

    sdgs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Header from '../../Components/Header/Header'
 
 function Home() {
   return (
     <div className='Home'>
-      <Header/>
+      
     </div>
   )
 }
