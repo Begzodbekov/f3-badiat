f8ac3951da689c01eaadc5bacaf68001da720a00
ziyo2007 <qodirjonovz6@gmail.com>
2023-09-11 14:58:57 +0500

    ffsaf

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Nasr from './pages/Nasr/Nasr';
+import Signup from './pages/Signup/Signup';
+
 function App() {
   return (
     <div className="App">
+
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/nasr' element ={<Nasr/>}></Route>
+        <Route path='/signup' element ={<Signup/>}></Route>
       </Routes>
+
     </div>
   );
 }
