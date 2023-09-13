f523317201f14c164e45c9dd02a8858d9df69262
namides <namides05@gmail.com>
2023-09-13 17:55:30 +0500

    add

---
+++ src/Components/BooksContent/BooksContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./BooksContent.scss";
+function BooksContent() {
+  return (
+    <div className='booksContent'>
+      
+    </div>
+  )
+}
+
+export default BooksContent
