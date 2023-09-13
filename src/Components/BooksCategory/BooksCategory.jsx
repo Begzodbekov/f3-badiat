68073376c535a965a737674122e5fa0807c7f8c3
namides <namides05@gmail.com>
2023-09-13 15:14:29 +0500

    add

--- src/Components/BooksCategory/BooksCategory.jsx
+++ src/Components/BooksCategory/BooksCategory.jsx
@@ -4,13 +4,13 @@ import "./BooksCategory.scss";
 function BooksCategory() {
   return (
     <div>
-      <ul className="book_categories">
+      {/* <ul className="book_categories">
         {bookData2?.map((item, index) => (
           <li key={index} className="book_categories__item">
             <img src={item.img} alt="img" />
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
