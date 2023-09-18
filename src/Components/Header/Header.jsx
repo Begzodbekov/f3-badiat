2a99dd42c8430e7fb9ec04246d1e048ebf9a3fbd
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-09-18 17:55:53 +0500

    sdghe

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -46,23 +46,23 @@ function Header() {
                                             Forum
                                         </Link>
                                     </li>
-                                    <li  className={location !='/signin' && '/' && 'signup' ? 'logins': 'login'}>
+                                    <li  className={location !='/'  ? 'logins': 'login'}>
                                         <Link to={'/signin'}>
                                               login
                                         </Link>
                                     </li>
-                                    <li  className={location !='/signin' && '/' && 'signup' ? 'logins': 'login'}>
+                                    <li  className={location != '/'? 'logins': 'login'}>
                                         <Link to={'/signup'}>
                                               register
                                         </Link>
                                     </li>
-                                    <li className={location !='/' ? 'odam': 'none'}>
+                                    <li className={location !='/' && '/signin' && '/signup' ? 'odam': 'none'}>
                                         <Link to={'/account'} >
                                               <img src="https://as1.ftcdn.net/v2/jpg/01/26/32/02/1000_F_126320234_AxO4v6wCAh4ZCKfQ9KbNH8YdBIA3XOQY.jpg" alt="" />
                                         </Link>
                                     </li>
-                                    <li className={location !='/' ? 'odam': 'none'} >
-                                        <Link>
+                                    <li className={location!='/' ? 'odam': 'none'} >
+                                        <Link to={'/account'}>
                                               F3-Team
                                         </Link>
                                     </li>
