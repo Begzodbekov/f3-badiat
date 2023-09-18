dbb0405ef4ae4d3471e580a23337e7ac01366bf2
odilbek2 <o3196181@gmail.com>
2023-09-18 16:39:21 +0500

    asdad

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Security from '../../Components/Security/Security'
+import Settings from '../../Components/Settings/Settings'
 
 function Profile() {
   return (
     <div className='Profile'>
         <Security/>
         {/* <Profile/> */}
+        <Settings/>
     </div>
   )
 }
