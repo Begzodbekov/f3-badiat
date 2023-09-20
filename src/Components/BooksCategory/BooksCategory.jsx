79a472470434ea47bf00af735f1807fdf2bf9583
ziyo2007 <qodirjonovz6@gmail.com>
2023-09-20 17:21:06 +0500

    ghgd

--- src/Components/BooksCategory/BooksCategory.jsx
+++ src/Components/BooksCategory/BooksCategory.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { bookData2 } from "../../lib/bookData2";
 import "./BooksCategory.scss";
 import { books } from "../../lib/bookData";
+import Herop from "../Herop/Herop";
 // import { bookData3 } from "../../lib/bookData3";
 
 function BooksCategory() {
+  // const Yurakref = useRef()
+  // console.log(Yurakref);
+  // const [array, setArray] = useState([])
+  // // const Array = []
+  // function fn (item){
+  //   // console.log(item.target);
+  //   array.push(Yurakref)
+  //   console.log(array);
+  // }
+  const [liked, setLiked ] = useState([])
+  console.log(liked);
   return (
     <div className="BooksCategory">
       <div className="container">
         
       <ul className="book_categories">
         {
-          books?.map((item, index) => (
+          books?.map((item, index ) => (
             <li key={index} className="book_item">
             <Link to={`/book-view/${item.id}`}>
               <img className="book_img" src={item.img} alt="img" />
@@ -20,16 +32,18 @@ function BooksCategory() {
               <span>{item.name}</span>
               <p>Otkir hoshimov</p>
               <div className="star">
-                
-                <p>❤️</p>
                 <div className="star_text">4.1 • 3400 ta fikrlar</div>
 
               </div>
             </Link>
+            <button onClick={()=>{
+              setLiked(item)
+            }} >❤️</button>
           </li>
         ))}
       </ul>
         </div>
+        {/* <Herop data={liked}/> */}
     </div>
   );
 }
