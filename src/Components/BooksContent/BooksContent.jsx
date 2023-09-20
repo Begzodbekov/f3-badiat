5699a7207eb045e1a7bff7f57f0d114b1590ca3a
namides <namides05@gmail.com>
2023-09-20 15:40:40 +0500

    add

--- src/Components/BooksContent/BooksContent.jsx
+++ src/Components/BooksContent/BooksContent.jsx
@@ -8,45 +8,46 @@ function BooksContent() {
   return (
     <div className="BooksContent">
       <div className="container">
+        <div className="big_category">
+        <div className="foot_link">
+          <div className="tag_link">Muallif haqida</div>
+          <div className="tag_link">Kitobdan iqtiboslar</div>
+          <div className="tag_link">Kitobxonlar taqrizi</div>
+        </div>
 
-      
-      <div className="foot_link">
-        <div className="tag_link">Muallif haqida</div>
-        <div className="tag_link">Kitobdan iqtiboslar</div>
-        <div className="tag_link">Kitobxonlar taqrizi</div>
-      </div>
-
-      <div>
         <div className="comment_tag">
-        <div className="comment_box">
-          <img src={tag} alt="" className="tag_img" />
-          <p className="comment">
-            Inson bolasi ne kunlarni ko‘rmaydi?! <br /> Har bir odam o‘z g‘ami bilan
-            bo‘lsa, hayotdan ko‘z yumib ketganlarga umr bo‘yi motam tutib o‘tsa,
-            bu moddiy olam shu kunlarga yetolarmidi? <br /> Hayot to‘lqini ojizlarni
-            qirg‘oqqa irg‘itib tashlaydi. Oqimga qarshi suza olganlar, to‘lqinni
-            egarlaganlargina ertangi kunga yetib keladi.
-          </p>
-          <div className="icon">
-            <img src={like} alt="" className="like" />
-            <img src={send} alt="" className="send" />
+          <div className="comment_box">
+            <img src={tag} alt="" className="tag_img" />
+            <p className="comment">
+              Inson bolasi ne kunlarni ko‘rmaydi?! <br /> Har bir odam o‘z g‘ami
+              bilan bo‘lsa, hayotdan ko‘z yumib ketganlarga umr bo‘yi motam
+              tutib o‘tsa, bu moddiy olam shu kunlarga yetolarmidi? <br /> Hayot
+              to‘lqini ojizlarni qirg‘oqqa irg‘itib tashlaydi. Oqimga qarshi
+              suza olganlar, to‘lqinni egarlaganlargina ertangi kunga yetib
+              keladi.
+            </p>
+            <div className="icon">
+              <img src={like} alt="" className="like" />
+              <img src={send} alt="" className="send" />
+            </div>
+          </div>
+
+          <div className="comment_box">
+            <img src={tag} alt="" className="tag_img" />
+            <p className="commentb">
+              Yer kurrasida chumolidek mehnat qilayotganlardan ko‘ra, tuproq
+              tagida <br /> yotganlar ko‘p. Yer qatlami odam suyaklariga to‘lib
+              ketgan.
+            </p>
+            <div className="icon">
+              <img src={like} alt="" className="like" />
+              <img src={send} alt="" className="send" />
+            </div>
           </div>
         </div>
-        
-        <div className="comment_box">
-          <img src={tag} alt="" className="tag_img" />
-          <p className="commentb">
-          Yer kurrasida chumolidek mehnat qilayotganlardan ko‘ra, tuproq tagida <br /> yotganlar ko‘p. Yer qatlami odam suyaklariga to‘lib ketgan.
-          </p>
-          <div className="icon">
-            <img src={like} alt="" className="like" />
-            <img src={send} alt="" className="send" />
-          </div>
         </div>
       </div>
     </div>
-    </div>
-      </div>
   );
 }
 
